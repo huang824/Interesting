adb2b8fb60d514e009b4be3e2f686d3cb67c8e07
Lexie Ritter <ritter14@purdue.edu>
2015-10-09 15:13:35 -0400

    add mod

--- calc.py
+++ calc.py
@@ -74,7 +74,7 @@ while (True):
             print "Quotient: ", div(a, b)
         elif (op == "**"):
 			print "Exponent: ", exp(a, b)
-		elif (op == "%"):
+        elif (op == "%"):
 			print "Modulus: ", mod(a, b)
         else:
             print "Invalid operation..."
